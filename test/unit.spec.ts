a73677bce45eeca59f065829af4b344ea6846d38
William Caine <william.caineiii@gmail.com>
2022-04-14 11:49:22 -0400

    removing test 'example' method

--- test/unit.spec.ts
+++ test/unit.spec.ts
@@ -1,9 +1,11 @@
 import { fixtures, getFixture } from '../test-utils'
-import { example, parseSync } from "../src"
+import { parseSync } from "../src"
 import expect from "expect"
 
+/*
 describe("unit", () => {
   it("smoke test", () => {
     expect(example()).toEqual(true)
   })
 })
+*/
